405f8d3f136d8b9c37455640062f0db992843137
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2023-09-12 17:44:16 +0200

    Face class added - build.sh updated

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #g++ main.cpp generators.cpp object3d.cpp rotation.cpp -o main.exe
 	
-g++ main.cpp cube.cpp thorus.cpp object3d.cpp rotation.cpp -o main.exe `sdl2-config --libs --cflags` -lSDL2_image -o sdl.exe
+g++ main.cpp face.cpp cube.cpp thorus.cpp object3d.cpp rotation.cpp -o main.exe `sdl2-config --libs --cflags` -lSDL2_image -o sdl.exe
 
