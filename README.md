3039e6291e723d86c337af88fc954cc872255b6c
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2024-02-08 15:44:00 +0100

    Readme added

---
+++ README.md
@@ -0,0 +1,6 @@
+# 3D objects generator
+Still it progress. 
+
+I develop it to have 3d objects for my Amiga OCS vector graphics renderer.
+
+![](example.png)
--- build.sh
+++
@@ -1,4 +0,0 @@
-#g++ main.cpp generators.cpp object3d.cpp rotation.cpp -o main.exe
-	
-g++ -g3 -std=c++1z main.cpp amigafile.cpp vector3d.cpp face.cpp cube.cpp thorus.cpp object3d.cpp rotation.cpp -o main.exe `sdl2-config --libs --cflags` -lSDL2_image -o sdl.exe
-
