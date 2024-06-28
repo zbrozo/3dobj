37ebb8f383a84b7324ca2753c44df62641883628
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2024-06-28 10:41:46 +0200

    Templates used in name creation and rotation

--- src/Rotation.cpp
+++ src/Rotation.cpp
@@ -1,34 +1,42 @@
 #include "Rotation.hpp"
-
-#include <type_traits>
 #include <cmath>
 
-#define PI 3.14159265
-#define radian (PI / 180.0)
+namespace
+{
+
+template <typename T> constexpr T pi = T(3.14159265);
+template <typename T> constexpr T radian = T(pi<T> / 180.0);
+
+auto getRadian(int degree)
+{
+  return degree * radian<double>;
+}
+
+} // namespace
 
 template<class T>
 T Rotation3D<T>::rotateX(T coord, int d)
 {
   const int x = coord.getX();
-  const int y = coord.getY() * cos(d * radian) - coord.getZ() * sin(d * radian);
-  const int z = coord.getY() * sin(d * radian) + coord.getZ() * cos(d * radian);
+  const int y = coord.getY() * cos(getRadian(d)) - coord.getZ() * sin(getRadian(d));
+  const int z = coord.getY() * sin(getRadian(d)) + coord.getZ() * cos(getRadian(d));
   return Vertex(x, y, z);
 }
 
 template<class T>
 T Rotation3D<T>::rotateY(T coord, int d)
 {
-  const int x = coord.getX() * cos(d * radian) + coord.getZ() * sin(d * radian);
+  const int x = coord.getX() * cos(getRadian(d)) + coord.getZ() * sin(getRadian(d));
   const int y = coord.getY();
-  const int z = coord.getZ() * cos(d * radian) - coord.getX() * sin(d * radian);
+  const int z = coord.getZ() * cos(getRadian(d)) - coord.getX() * sin(getRadian(d));
   return Vertex(x, y, z);
 }
 
 template<class T>
 T Rotation3D<T>::rotateZ(T coord, int d)
 {
-  const int x = coord.getX() * cos(d * radian) - coord.getY() * sin(d * radian);
-  const int y = coord.getX() * sin(d * radian) + coord.getY() * cos(d * radian);
+  const int x = coord.getX() * cos(getRadian(d)) - coord.getY() * sin(getRadian(d));
+  const int y = coord.getX() * sin(getRadian(d)) + coord.getY() * cos(getRadian(d));
   const int z = coord.getZ();
   return Vertex(x, y, z);
 }
